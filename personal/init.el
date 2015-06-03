be1d96314a2229d0a456b87f7bf125fbd2aa4cdc
laohyx <laohyx@163.com>
2015-06-03 15:17:04 +0800

    Fix fonts width in Chinese environment.
    Add postgresql C style

--- personal/init.el
+++ personal/init.el
@@ -88,3 +88,18 @@
 (setq org-support-shift-select t)
 
 (prelude-require-package 'ag)
+
+(prelude-require-package 'key-chord)
+(require 'key-chord)
+(key-chord-define-global "mk" 'set-mark-command)
+
+
+(defun company-yasnippet-or-completion ()
+  (interactive)
+  (let ((yas-fallback-behavior nil))
+    (unless (yas-expand)
+      (call-interactively #'company-complete-common))))
+(add-hook 'company-mode-hook (lambda ()
+                               (substitute-key-definition 'company-complete-common
+                                                          'company-yasnippet-or-completion
+                                                          company-active-map)))
